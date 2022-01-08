d2a45efb7e74e99baaee0c95f964a2c387e945be
Nicholas <zhangchangsheng01@gmail.com>
2022-01-08 23:35:35 +0800

    修改 application.properties port 自定义WebMVC配置 实现跨域

--- src/main/java/com/nicholas/music/config/WebMvcConfiguration.java
+++ src/main/java/com/nicholas/music/config/WebMvcConfiguration.java
@@ -1,2 +1,24 @@
-package com.nicholas.music.config;public class WebMvcConfiguration {
+package com.nicholas.music.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+    /**
+     * 配置跨域,允许所有
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
 }
